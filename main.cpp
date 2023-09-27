b35acdd2cc08bbcedf2042c683530402aaea4219
sandrew338 <andrey.nazarkevych@gmail.com>
2023-09-27 23:44:10 +0300

    qwer

---
+++ main.cpp
@@ -0,0 +1,28 @@
+// Lab_03_4.cpp
+// < Назаркевич, Андрйі >
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 17
+#include <iostream>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+	double y; // вхідний параметр
+	double R;
+	do
+	{
+		cout << "R = ";
+		cin >> R;
+	} while (R <= 0);
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+
+	// розгалуження в повній формі
+	if ((y <= sqrt(R*R - pow(x + R, 2)) && y >= 0 && y <= R && x >= -R && x <= 0) || (y >= -sqrt(R*R - pow(x - R, 2)) && y >= -R && y <= 0 && x >= 0 && x <= R))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	cin.get();
+	return 0;
+}
